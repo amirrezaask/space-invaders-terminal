3289bc22f775597fdcf1508bb14458797ea3621c
amirrezaask <raskarpour@gmail.com>
2022-08-07 22:36:59 +0430

    enemy rendering

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,75 @@
 use ncurses::*;
 
 const WELCOME: &str = "welcome to space invaders";
-const PLAYER_SHIP: &str = "&&&&";
+const PLAYER_SHIP: &'static str = "YOUR SHIP";
+const ENEMY1_SHIP: &'static str = "@@";
+const KEY_SPACE: i32 = ' ' as i32;
+const KEY_EXIT: i32 = 'q' as i32;
 
+struct Rocket {
+    pos: Position,
+    shape: &'static str,
+}
+
+#[derive(Debug)]
 struct Position {
     x: i32,
     y: i32,
 }
-
-impl Position {
-    pub fn up(&mut self) {
-        self.y -= 1;
-    }
-    pub fn down(&mut self) {
-        self.y += 1;
-    }
-    pub fn right(&mut self) {
-        self.x += 1;
-    }
-    pub fn left(&mut self) {
-        self.x -= 1;
-    }
-
+#[derive(Debug)]
+struct Ship {
+    pos: Position,
+    shape: &'static str,
 }
 
+impl Ship {
+    pub fn draw(&self) {
+        wmove(stdscr(), self.pos.y, self.pos.x);
+        addstr(&self.shape);
+    }
+
+    pub fn up(&mut self) {
+        self.pos.y -= 1;
+    }
+    pub fn down(&mut self) {
+        self.pos.y += 1;
+    }
+    pub fn right(&mut self) {
+        self.pos.x += 1;
+    }
+    pub fn left(&mut self) {
+        self.pos.x -= 1;
+    }
+}
+#[derive(Debug)]
 struct Game {
     max_height: i32,
     max_width: i32,
 
-    player_pos: Position,
+    player: Ship,
+    enemies: Vec<Ship>,
     msg: Option<String>,
-    done: bool
+    done: bool,
+}
+
+fn create_enemy_grid(max_height: i32, max_width: i32, num_rows: i32, num_cols: i32) -> Vec<Ship> {
+    let mut ships = Vec::<Ship>::new();
+    let enemy_x_delta = 2;
+    let x_offset =
+        (max_width - ((enemy_x_delta * (num_cols - 1)) + (num_cols * ENEMY1_SHIP.len() as i32))) / 2;
+    for i in 0..num_rows {
+        for j in 0..num_cols {
+            ships.push(Ship {
+                pos: Position {
+                    x: x_offset + (j * enemy_x_delta) + (j-1 * ENEMY1_SHIP.len() as i32),
+                    y: (max_height / 8) + i as i32,
+                },
+                shape: ENEMY1_SHIP,
+            })
+        }
+    }
+
+    ships
 }
 
 impl Game {
@@ -42,14 +81,20 @@ impl Game {
         keypad(stdscr(), true);
 
         getmaxyx(stdscr(), &mut max_height, &mut max_width);
-
-        Self {
-            max_height,
-            max_width,
-            player_pos: Position {
+        let player = Ship {
+            pos: Position {
                 x: (max_width - PLAYER_SHIP.len() as i32) / 2,
                 y: (max_height - max_height / 6),
             },
+            shape: PLAYER_SHIP,
+        };
+
+        let enemies: Vec<Ship> = create_enemy_grid(max_height, max_width, 5, 8);
+        Self {
+            max_height,
+            max_width,
+            player,
+            enemies,
             msg: None,
             done: false,
         }
@@ -63,11 +108,16 @@ impl Game {
         if self.msg.is_some() {
             self.print_center(self.msg.clone().unwrap().as_str());
             self.msg = None;
-            return;
+            getch();
+            self.clear();
         }
         // draw player
-        wmove(stdscr(), self.player_pos.y, self.player_pos.x);
-        addstr(PLAYER_SHIP);
+        self.player.draw();
+
+        //draw enemies
+        for enemy in self.enemies.iter_mut() {
+            enemy.draw();
+        }
     }
 
     pub fn print_center(&self, text: &str) {
@@ -88,22 +138,26 @@ impl Game {
                 break;
             }
             self.render();
-            
+
             let player_move = getch();
 
             match player_move {
                 KEY_LEFT => {
-                    self.player_pos.left();
+                    self.player.left();
                 }
                 KEY_RIGHT => {
-                    self.player_pos.right();
+                    self.player.right();
                 }
                 KEY_UP => {
-                    self.player_pos.up();
+                    self.player.up();
                 }
                 KEY_DOWN => {
-                    self.player_pos.down();
+                    self.player.down();
                 }
+                KEY_SPACE => {
+                    // self.player.shoot();
+                }
+                KEY_EXIT => break,
                 _ => {
                     self.msg = Some("unkown key".to_string());
                 }
@@ -115,4 +169,5 @@ impl Game {
 fn main() {
     let mut game = Game::new();
     game.start();
+    println!("\n\n{:?}", game);
 }
